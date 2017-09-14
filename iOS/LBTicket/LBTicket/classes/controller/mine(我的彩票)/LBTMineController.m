656ebf8efc498ddea107ea702aa0478ba87d8c28
CodeJCSON <2334497007@qq.com>
2017-09-14 12:00:01 +0800

    提交

--- iOS/LBTicket/LBTicket/classes/controller/mine(我的彩票)/LBTMineController.m
+++ iOS/LBTicket/LBTicket/classes/controller/mine(我的彩票)/LBTMineController.m
@@ -7,31 +7,54 @@
 //
 
 #import "LBTMineController.h"
-
+#import "UIImage+Fit.h"
 @interface LBTMineController ()
-
+@property (strong, nonatomic) UIButton *loginBtn;
+@property (strong, nonatomic) UIImageView *imageview;
 @end
 
 @implementation LBTMineController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // 背景色
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg"]];
+    // 设置上部视图
+    [self setUpTopImgaeView];
+    // 登录按钮
+    [self setUpLoginButton];
 }
+// 设置上部视图
+- (void)setUpTopImgaeView{
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIImageView *imageview = [[UIImageView alloc]init];
+    _imageview = imageview;
+    imageview.image = [UIImage imageNamed:@"LoginScreen"];
+    [self.view addSubview:imageview];
+    [imageview mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@20);
+        make.left.equalTo(@10);
+        make.right.equalTo(self.view).offset(-10);
+        make.height.equalTo(@140);
+    }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// 设置登录按钮
+- (void)setUpLoginButton{
+    _loginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_loginBtn setBackgroundImage:[UIImage resizeImage:@"RedButton"] forState:UIControlStateNormal];
+    [_loginBtn setBackgroundImage:[UIImage resizeImage:@"RedButtonPressed"] forState:UIControlStateHighlighted];
+    [_loginBtn addTarget:self action:@selector(loginButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [_loginBtn setTitle:@"登录" forState:UIControlStateNormal];
+    [self.view addSubview:_loginBtn];
+    [_loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(_imageview.mas_bottom).offset(20);
+        make.width.equalTo(@([UIScreen cz_screenWidth] - 40));
+        make.height.equalTo(@40);
+    }];
 }
-*/
-
+// 点击
+- (void)loginButtonClick{
+    LBTLog(@"登录");
+}
 @end
