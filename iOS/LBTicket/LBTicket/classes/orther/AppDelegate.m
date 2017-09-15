e5185741642aec0683264faf39c0707690400955
CodeJCSON <2334497007@qq.com>
2017-09-15 19:00:52 +0800

    提交

--- iOS/LBTicket/LBTicket/classes/orther/AppDelegate.m
+++ iOS/LBTicket/LBTicket/classes/orther/AppDelegate.m
@@ -24,6 +24,44 @@
     self.window.rootViewController = [[LBTTabBarController alloc]init];
     return YES;
 }
+- (NSString *)digView:(UIView *)view
+{
+    if ([view isKindOfClass:[UITableViewCell class]]) return @"";
+    
+    NSMutableString *xml = [NSMutableString string];
+    
+    /// 标签开头
+    [xml appendFormat:@"<%@ frame=\"%@\"", view.class, NSStringFromCGRect(view.frame)];
+    if (!CGPointEqualToPoint(view.bounds.origin, CGPointZero)) {
+        [xml appendFormat:@" bounds=\"%@\"", NSStringFromCGRect(view.bounds)];
+    }
+    
+    if ([view isKindOfClass:[UIScrollView class]]) {
+        UIScrollView *scroll = (UIScrollView *)view;
+        if (!UIEdgeInsetsEqualToEdgeInsets(UIEdgeInsetsZero, scroll.contentInset)) {
+            [xml appendFormat:@" contentInset=\"%@\"", NSStringFromUIEdgeInsets(scroll.contentInset)];
+        }
+    }
+    
+    /// 判断是否要结束
+    if (view.subviews.count == 0) {
+        [xml appendString:@" />"];
+        return xml;
+    } else {
+        [xml appendString:@">"];
+    }
+    
+    /// 遍历所有的子控件
+    for (UIView *child in view.subviews) {
+        NSString *childXml = [self digView:child];
+        [xml appendString:childXml];
+    }
+    
+    /// 标签结尾
+    [xml appendFormat:@"</%@>", view.class];
+    
+    return xml;
+}
 
 
 
