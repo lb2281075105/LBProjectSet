d33cf4f4df127b72167825b1a5f8e40556af7700
CodeJCSON <2334497007@qq.com>
2017-09-15 18:30:11 +0800

    设置中分享实现

--- iOS/LBTicket/LBTicket/classes/controller/mine(我的彩票)/setting/LBTSettingController.m
+++ iOS/LBTicket/LBTicket/classes/controller/mine(我的彩票)/setting/LBTSettingController.m
@@ -11,6 +11,7 @@
 #import "LBTHelpController.h"
 #import "LBTProductController.h"
 #import "LBTAboutController.h"
+#import "LBTShareController.h"
 @interface LBTSettingController ()
 
 @end
@@ -29,15 +30,15 @@
 /// 添加第0组的模型数据
 - (void)add0SectionItems
 {
-    // 1.1.推送和提醒
+    /// 推送和提醒
     LBTSettingArrowItem *push = [LBTSettingArrowItem itemWithIcon:@"MorePush" title:@"推送和提醒"];
     push.showVCClass = [LBTPushNoticeController class];
     
-    // 1.2.摇一摇机选
+    /// 摇一摇机选
     LBTSettingSwitchItem *shake = [LBTSettingSwitchItem itemWithIcon:@"handShake" title:@"摇一摇机选"];
     // shake.key = LBTSettingShakeChoose;
     
-    // 1.3.声音效果
+    /// 声音效果
     LBTSettingSwitchItem *sound = [LBTSettingSwitchItem itemWithIcon:@"sound_Effect" title:@"声音效果"];
     // sound.key = LBTSettingSoundEffect;
     
@@ -47,29 +48,29 @@
 /// 添加第1组的模型数据
 - (void)add1SectionItems
 {
-    // 2.1.检查新版本
+    /// 检查新版本
     LBTSettingArrowItem *update = [LBTSettingArrowItem itemWithIcon:@"MoreUpdate" title:@"检查新版本"];
     update.itemClick = ^{
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"目前已是最新版本了" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
         [alert show];
     };
     
-    // 2.2.帮助
+    /// 帮助
     LBTSettingArrowItem *help = [LBTSettingArrowItem itemWithIcon:@"MoreHelp" title:@"帮助"];
     help.showVCClass = [LBTHelpController class];
     
-    // 2.3.分享
+    /// 分享
     LBTSettingArrowItem *share = [LBTSettingArrowItem itemWithIcon:@"MoreShare" title:@"分享"];
-    // share.showVCClass = [ILShareViewController class];
+    share.showVCClass = [LBTShareController class];
     
-    // 2.4.查看消息
+    /// 查看消息
     LBTSettingArrowItem *msg = [LBTSettingArrowItem itemWithIcon:@"MoreMessage" title:@"查看消息"];
     
-    // 2.5.产品推荐
+    /// 产品推荐
     LBTSettingArrowItem *product = [LBTSettingArrowItem itemWithIcon:@"MoreNetease" title:@"产品推荐"];
     product.showVCClass = [LBTProductController class];
     
-    // 2.6.关于
+    /// 关于
     LBTSettingArrowItem *about = [LBTSettingArrowItem itemWithIcon:@"MoreAbout" title:@"关于"];
     about.showVCClass = [LBTAboutController class];
     
