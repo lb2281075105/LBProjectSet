0896f56aa529ead15eb5a3f07f555de1ad55e40e
liubo <liubo@LiuBo-Mac.local>
2017-10-20 01:22:58 +0800

    提交

---
+++ h5/s/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>jQuery WeUI</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <meta name="description" content="">
+
+    <!-- 不能动的css   -->
+    <link rel="stylesheet" href="css/weui.css">
+    <link rel="stylesheet" href="css/jquery-weui.css">
+    <link rel="stylesheet" href="css/demos.css">
+
+
+    <!-- 扩展css   不能动 -->
+    <link rel="stylesheet" href="css/extend.css">
+
+    <!-- 辅助css 不能动 -->
+    <link rel="stylesheet" href="css/help.css">
+
+    <!-- 每个组件或者页面独立的样式，整合好后才可以放到扩展里  -->
+    <link rel="stylesheet" href="css/s.css">
+    <!--  字体  -->
+    <link rel="stylesheet" href="css/demo.css">
+    <script src="css/iconfont.js"></script>
+
+
+    <script src="js/jquery-2.1.4.js"></script>
+    <script src="js/fastclick.js"></script>
+
+    <script src="js/jquery-weui.js"></script>
+
+
+</head>
+
+<body ontouchstart>
---
+++ h5/s/topIcon.php
@@ -0,0 +1,78 @@
+<?php include "header.php" ?>
+
+
+<div class="weui-cells__title">特色服务模块</div>
+<div class="weui-cells">
+<div class="weui-cell-topIcon-style">
+         <div class="weui-cell-topIcon">
+             <img src="images/topIcon.png" class="weui-cells-top-images">
+          </div>
+</div>
+          <div class="weui-cell-bottom">
+	
+	<div class="f-gray">
+        <img src="images/签证.png" style="width: 25px;height: 25px;">
+        <div class="f-13 pdb-5">代理签证</div>
+  </div>
+
+  <div class="f-gray">
+        <img src="images/留学.png" style="width: 25px;height: 25px;">
+        <div class="f-13 pdb-5">留学快汇</div> 
+  </div>
+
+  <div class="f-gray">
+        <img src="images/结汇.png" style="width: 25px;height: 25px;">
+        <div class="f-13 pdb-5">外汇结汇</div>    
+  </div>
+
+  <div>
+      
+  <div class="f-gray">
+        <img src="images/购汇.png" style="width: 25px;height: 25px;">
+        <div class="f-13 pdb-5">外汇购汇</div>      
+  </div>
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+<!-- 分割 -->
+<div class="weui-cells__title weui-cell-top-nav">品质购团<span class="weui-cell-more f-gray">更多</span></div>
+<div class="weui-cells">
+
+    <div class="weui-cell-topIcon-style">
+         <div class="weui-cell-topIcon">
+             <img src="images/topIcon1.png" class="weui-cells-top-images">
+          </div>
+    </div>
+    
+ <div class="weui-cell-bottom-pd mglr-10">
+
+   <!-- <div style="overflow: hidden;">
+        <img src="images/topIcon1.png">
+    </div> -->
+    <div class="weui-cell-bottom-image">
+      <div class="weui-cell-bottom-bootom1">
+             <img src="images/bottom1.png">
+      </div>
+      <div class="bg-white" style="width: 10px;"></div>
+      <div class="weui-cell-bottom-bootom1" >
+             <img src="images/bottom2.png">
+      </div>
+      <div style="width: 10px;" class="bg-white"></div>
+      <div class="weui-cell-bottom-bootom1">
+             <img src="images/bottom3.png">
+      </div>
+    </div>
+ </div>
+
+</div>
+
+
+
+<?php include "footer.php"?>
