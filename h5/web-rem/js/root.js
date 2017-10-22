f28b411b2d3f2afd571a4f5bd80539a3b30d8a44
liubo <liubo@LiuBo-Mac.local>
2017-10-22 23:30:55 +0800

    提交

---
+++ h5/web-rem/js/root.js
@@ -0,0 +1,11 @@
+(function () {
+	
+
+	var calc = function(){
+		var docuElement = document.documentElement;
+		var clientWidthValue = docuElement.clientWidth > 750 ? 750 :docuElement.clientWidth;
+		docuElement.style.fontSize = 20 * (clientWidthValue / 375) + "px";
+	}
+	calc();
+	window.addEventListener("resize",calc);
+})();
