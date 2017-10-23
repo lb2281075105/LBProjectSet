ab32909de3788187055256bbcc0224713a5d9382
liubo <liubo@LiuBo-Mac.local>
2017-10-24 00:24:37 +0800

    ajax

---
+++ h5/ajax/file.php
@@ -0,0 +1,6 @@
+<?php
+
+var_dump($_FILES);
+header('content-type:text/html; charset= utf-8');
+// sleep(5);
+// move_uploaded_file($_FILES['picture']['tmp_name'], './upload/'.$_FILES['picture']['name']);
