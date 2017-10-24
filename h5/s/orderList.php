6257d82d0f472b035f669cb976fd5621b55488a9
CodeJCSON <2334497007@qq.com>
2017-10-24 19:44:07 +0800

    提交

--- h5/s/orderList.php
+++ h5/s/orderList.php
@@ -6,6 +6,7 @@
       <h1 class="demos-title">订单列表</h1>
     </header>
 
+   
     <div class="bd">
       <div class="page__bd">
 
@@ -19,23 +20,18 @@
           </a>
         </div>
         <div style="background-color: white;">
-           <div class="weui-cell-top height-80">
-                <div class="weui-cell-float-left w-40">
-                   <i class="iconfont iconfontstyle">&#xe606;</i>
-           	    </div>
-           	    <div class="weui-cell-top-right">
-           	        <div class="weui-cell-peisong height-40">
-           	        	<div class="weui-cell-yuan"><span class="weui-cell-span-bottom">配送员已取货</span></div><div class="weui-cell-time width-56"><span class="weui-cell-span-bottom">16:54</span></div>
-           	        </div>
-           	        <div class="height-40">配送员电话13838383838</div>
-           	    </div>
-           	    <div style="clear: both;"></div>
-           </div>
-           <div class="weui-cell-top height-60">
-                <div class="weui-cell-float-left w-40">
-                   <i class="iconfont iconfontstyle">&#xe606;</i>
-           	    </div>
-           	    <div class="weui-cell-flex-haha" style="background-color: gray">
+           <div class="weui-cell-top">
+                <div class="weui-cell-float-left w-40" style="height: 180px;background-color: red">
+                  <!-- <div style="height: 60px;width: 40px;" style="background-color: green"></div> -->
+                   
+                    <img class="iconfontstyle" src="images/qian.png">
+                </div>
+                <div class="weui-cell-top-right">
+                    <div class="weui-cell-peisong height-40">
+                      <div class="weui-cell-yuan"><span class="weui-cell-span-bottom">配送员已取货</span></div><div class="weui-cell-time width-56"><span class="weui-cell-span-bottom">16:54</span></div>
+                    </div>
+                    <div class="height-40">请在提交订单后1小时内支付</div>
+                    <div class="weui-cell-flex-haha height-60" style="background-color: gray">
                    <div class="weui-cell-flex-left weui-cell-flex-jjj">
                      <img src="images/留学.png" class="pdl-10 wh-35">
                      <img src="images/留学.png" class="pdl-10 wh-35">
@@ -50,11 +46,91 @@
                       <div>￥58.00</div>
                       <div>共50件</div>
                    </div>
-           	                          
+                                      
                    </div>
-           	    </div>
-           	    <div style="clear: both;"></div>
-           	</div>
+                   <div class="weui-cell-top height-40" style="background-color: white;">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-flex-haha">
+                   <div class="weui-cell-flex-left weui-cell-shijian">
+                     <span class="mat f-14">2015-12-26 12:26</span>
+                   </div>
+                   <div style="text-align: center;" class="weui-cell-right-money">
+                      <div class="border pad"></div>
+                   </div>            
+                   </div>
+                </div>
+                <div style="clear: both;"></div>
+            </div>
+                </div>
+                <!-- <div style="clear: both;"></div> -->
+                 <!-- <div class="weui-cell-top height-60"> -->
+                <!-- <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div> -->
+                
+                <!-- </div> -->
+                <!-- <div style="clear: both;"></div> -->
+            </div>
+             
+           </div>
+          
+        </div>
+        
+      </div>
+    </div>
+
+<div class="bd">
+      <div class="page__bd">
+
+        <div class="weui-cells">
+          <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+              <span>鲜家生鲜精品馆</span>
+              <i class="iconfont iconfontstyle">&#xe6b8;</i>
+            </div>
+            <div class="weui-cell-right">待支付</div>
+          </a>
+        </div>
+        <div style="background-color: white;">
+           <div class="weui-cell-top height-80">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-top-right">
+                    <div class="weui-cell-peisong height-40">
+                      <div class="weui-cell-yuan"><span class="weui-cell-span-bottom">等待支付</span></div><div class="weui-cell-time width-56"><span class="weui-cell-span-bottom">16:54</span></div>
+                    </div>
+                    <div class="height-40">配送员电话13838383838</div>
+                </div>
+                <div style="clear: both;"></div>
+           </div>
+           <div class="weui-cell-top height-60">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-flex-haha" style="background-color: gray">
+                   <div class="weui-cell-flex-left weui-cell-flex-jjj">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <i class="iconfont iconfontstyle pdl-10 black f-5 width-10">&#xe614;</i>
+                     <i class="iconfont iconfontstyle pdl-2 black f-5 width-10">&#xe614;</i>
+                     <i class="iconfont iconfontstyle pdl-2 black f-5 width-10">&#xe614;</i>
+
+                   </div>
+                   <div style="text-align: center;" class="weui-cell-right-money width-56" style="padding-top: 0px;">
+                      <div>￥58.00</div>
+                      <div>共50件</div>
+                      <div>达达专送</div>
+                   </div>
+                                      
+                   </div>
+                </div>
+                <div style="clear: both;"></div>
+            </div>
              <div class="weui-cell-top height-40" style="background-color: white;">
                 <div class="weui-cell-float-left w-40">
                    <i class="iconfont iconfontstyle">&#xe606;</i>
@@ -75,7 +151,75 @@
       </div>
     </div>
 
+<div class="bd">
+      <div class="page__bd">
 
+        <div class="weui-cells">
+          <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+              <span>鲜家生鲜精品馆</span>
+              <i class="iconfont iconfontstyle">&#xe6b8;</i>
+            </div>
+            <div class="weui-cell-right">进行中</div>
+          </a>
+        </div>
+        <div style="background-color: white;">
+           <div class="weui-cell-top height-80">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-top-right">
+                    <div class="weui-cell-peisong height-40">
+                      <div class="weui-cell-yuan"><span class="weui-cell-span-bottom">订单配送中</span></div><div class="weui-cell-time width-56"><span class="weui-cell-span-bottom">16:54</span></div>
+                    </div>
+                    <div class="height-40">配送员电话13838383838</div>
+                </div>
+                <div style="clear: both;"></div>
+           </div>
+           <div class="weui-cell-top height-60">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-flex-haha" style="background-color: gray">
+                   <div class="weui-cell-flex-left weui-cell-flex-jjj">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <img src="images/留学.png" class="pdl-10 wh-35">
+                     <i class="iconfont iconfontstyle pdl-10 black f-5 width-10">&#xe614;</i>
+                     <i class="iconfont iconfontstyle pdl-2 black f-5 width-10">&#xe614;</i>
+                     <i class="iconfont iconfontstyle pdl-2 black f-5 width-10">&#xe614;</i>
+
+                   </div>
+                   <div style="text-align: center;" class="weui-cell-right-money width-56" style="padding-top: 0px;">
+                      <div>￥58.00</div>
+                      <div>共50件</div>
+                      <div>达达专送</div>
+                   </div>
+                                      
+                   </div>
+                </div>
+                <div style="clear: both;"></div>
+            </div>
+             <div class="weui-cell-top height-40" style="background-color: white;">
+                <div class="weui-cell-float-left w-40">
+                   <i class="iconfont iconfontstyle">&#xe606;</i>
+                </div>
+                <div class="weui-cell-flex-haha">
+                   <div class="weui-cell-flex-left weui-cell-shijian">
+                     <span class="mat f-14">2015-12-26 12:26</span>
+                   </div>
+                   <div style="text-align: center;" class="weui-cell-right-money">
+                      <div class="border pad"></div>
+                   </div>            
+                   </div>
+                </div>
+                <div style="clear: both;"></div>
+            </div>
+        </div>
+        
+      </div>
+    </div>
 
 
 <?php include "footer.php"?>
